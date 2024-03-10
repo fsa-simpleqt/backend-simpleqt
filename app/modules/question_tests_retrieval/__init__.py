f03a9eea1fd9b05c463da22773781e35bd8d1e64
HuyDN <huydao2802@gmail.com>
2024-03-11 00:51:35 +0700

    Phase1/HuyDN: Update phase 1

--- app/modules/question_tests_retrieval/__init__.py
+++ app/modules/question_tests_retrieval/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, UploadFile, File
 from typing import Annotated
 
 from app.modules.question_tests_retrieval.models.jd2text import jobdes2text
-from app.modules.question_tests_retrieval.models.text2tector import text2vector
+# from app.modules.question_tests_retrieval.models.text2tector import text2vector
 from app.modules.question_tests_retrieval.models.question_tests_logic import get_question_test
 
 qtretrieval_router = APIRouter(prefix="/qtretrieval", tags=["qtretrieval"])
@@ -15,15 +15,12 @@ async def index():
 # only upload .txt file
 async def send_jd(txt_file: Annotated[UploadFile, File(..., description="The JD file", media_type=["text/plain"])]):
     try:
-        # # read the txt file with format
-        # jobdes = txt_file.file.read().decode("utf-8")
-        # sumaryjd_text = jobdes2text(jobdes)
-        # print("sumaryjd_text: ", sumaryjd_text)
-        # sumaryjd_vector = text2vector(sumaryjd_text)
-        # print("sumaryjd_vector: ", sumaryjd_vector)
-        text = "Job Title is Senior AI Engineer, Level is Senior, and Brief summary of required skills is NLP, experiencing in using containers"
-        if get_question_test(text):
-            return {"message": "Send JD successfully and get question test successfully"}
+        # read the txt file with format
+        jobdes = txt_file.file.read().decode("utf-8")
+        sumaryjd_text = jobdes2text(jobdes)
+        if get_question_test(sumaryjd_text):
+            return {"message": "Send JD successfully and get question test successfully",
+                    "sumaryjd_text": sumaryjd_text}
         else:
             return {"message": "Error"}
     except Exception as e:
