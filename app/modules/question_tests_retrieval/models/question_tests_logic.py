2545466c906eaf045b358b212b56b8f4f3b37860
HuyDN <huydao2802@gmail.com>
2024-03-15 10:46:00 +0700

    Phase1/HuyDN: Updade get_question_tests function

--- app/modules/question_tests_retrieval/models/question_tests_logic.py
+++ app/modules/question_tests_retrieval/models/question_tests_logic.py
@@ -21,7 +21,7 @@ GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY, request_timeout=120)
 gemini_evaluator = load_evaluator("embedding_distance", distance_metric=EmbeddingDistance.COSINE, embeddings=embedding_model)
     
-def compare_vector(description_vector, max_number_of_points=3):
+def compare_vector(description_vector, max_number_of_points=1):
     similarity_list = qdrant_client.search(
         collection_name="question_tests",
         query_vector=description_vector,
@@ -45,7 +45,6 @@ def download_question_test(question_test_url_list: list):
         name_bucket = url.split(f"gs://{firebase_bucket.name}/")[1]
         blob = firebase_bucket.blob(name_bucket)
         blob.download_to_filename(f'data/question_tests/{name_bucket}')
-
     return True
 
 def get_question_tests(text: str):
@@ -55,9 +54,13 @@ def get_question_tests(text: str):
     question_test_url_list = []
     for point in formatted_similarity_list:
         id = point.get("id")
-        question_test_url_list.append(get_question_test_by_id(id).get("question_tests_url"))
+        question_tests_url = get_question_test_by_id(id).get("question_tests_url")
+        match_score = point.get("score")
+        question_test_url_list.append({"id": id, "question_tests_url": question_tests_url, "match_score": match_score})
 
-    if download_question_test(question_test_url_list):
-        return True
-    else:
-        return False
+    # if download_question_test(question_test_url_list):
+    #     return True
+    # else:
+    #     return False
+    
+    return question_test_url_list
