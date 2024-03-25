199c43cf0256466eec009fe75b78012ef154d12f
HuyDN <huydao2802@gmail.com>
2024-03-25 13:44:12 +0700

    HuyDN: Add CORS

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
 from app.modules import modules_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(modules_router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def index():
