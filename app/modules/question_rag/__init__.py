a4179f1844d8cc2ac4691c4d6c7001fc228993b5
WangDinh <quangdinh2102@gmail.com>
2024-03-26 14:41:16 +0700

    Phase3/QuangDT: Edit gen quiz

--- app/modules/question_rag/__init__.py
+++ app/modules/question_rag/__init__.py
@@ -4,13 +4,14 @@ from typing import Annotated
 from app.modules.question_rag.models.question_rag_logic import question_rag
 from app.modules.question_tests_retrieval.models.jd2text import jobdes2text
 from app.modules.crud_jds.models.crud_jds import get_jd_text_by_id
-from app.modules.crud_rag_question_tests.models.crud_rag_question_tests import download_file_rag_question_tests
+from app.modules.crud_rag_question_tests.models.crud_rag_question_tests import download_file_rag_question_tests, get_all_rag_question_tests
 
 quiz_gen_router = APIRouter(prefix="/quiz_gen", tags=["quiz_gen"])
 
 @quiz_gen_router.get("/")
 async def index():
-    return {"message": "Welcome to quiz generator page"}
+    data = get_all_rag_question_tests()
+    return data
 
 @quiz_gen_router.post("/quiz_gen")
 async def quiz_gen(id_jd: str):
