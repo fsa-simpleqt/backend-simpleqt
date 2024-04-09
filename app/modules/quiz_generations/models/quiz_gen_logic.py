49cc1906eb96192fffc929dad9c8434331f7cd24
WangDinh <quangdinh2102@gmail.com>
2024-04-09 10:17:15 +0700

    Phase3/QuangDT: Add gen quiz

--- app/modules/quiz_generations/models/quiz_gen_logic.py
+++ app/modules/quiz_generations/models/quiz_gen_logic.py
@@ -24,39 +24,4 @@ QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY")
 QDRANT_URL = os.environ.get("QDRANT_URL")
 
 def generate_question(jobtext: str):
-    llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY, request_timeout=120)
-
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-
-    client = qdrant_client.QdrantClient(
-        url=QDRANT_URL,
-        api_key=QDRANT_API_KEY,
-    )
-    
-    doc_store = Qdrant(
-        client=client,
-        collection_name="rag_documents_test",
-        embeddings=embeddings,
-    )
-
-    json_parser = JsonOutputParser()
-
-    prompt = ChatPromptTemplate.from_template("""
-    Answer the question based only on the following context:                                
-    <context>
-    {context}
-    </context>
-                                                                            
-    Generate a 10 quiz suitable for the given job description "{input}". Do not include "All of the above" answers.
-    Output format is JSON:
-    ("__count__": 10, "data": ( "id": "", "question": "", "choices": [ "A. ", "B. ", "C.", "D. " ], "explanation": "", "answer": "", "level": "", "domain": "" )).
-    About level help me three levels: "Fresher, Junior, Senior".
-    """)
-
-    document_chain = create_stuff_documents_chain(llm, prompt, output_parser=json_parser)
-
-    retriever = doc_store.as_retriever()
-    retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    response = retrieval_chain.invoke({"input": jobtext})
-
-    return response["answer"]
+    pass
